2ab4842fab6462c7470c1af56285a0704b40cb56
Elizabeth Pachus <epachus@college.harvard.edu>
2019-11-19 11:44:28 -0500

    End of work period, fixed bug w graph 1

--- create_graphics.R
+++ create_graphics.R
@@ -3,20 +3,41 @@ library(maps)
 library(ggthemes)
 library(janitor)
 library(openintro)
+library(scales)
+library(infer)
+library(broom)
 library(tidyverse)
 
 
 # READING DATA INTO OBJECTS
 
 final_data <- read_csv("raw-data/final_data.csv")
+final_data_map <- read_csv("raw-data/final_data_map.csv")
 
-us_states <- map_data("state") %>% 
-  mutate(region = state2abbr(region)) %>% 
-  mutate(state_name = abbr2state(region)) %>% 
-  select(-region, -subregion)
+# us_states <- map_data("state") %>% 
+#   mutate(region = state2abbr(region)) %>% 
+#   mutate(state_name = abbr2state(region)) %>% 
+#   select(-region, -subregion)
+# 
+# 
+# final_data_map <- full_join(final_data, us_states)
+# 
+# write_csv(final_data_map, "raw-data/final_data_map.csv")
+# 
+# suicide_data <- read_csv("raw-data/suicide_by_state.csv") %>% 
+#   clean_names() %>% 
+#   mutate(suicide_rate = rate) %>% 
+#   mutate(suicide_deaths = deaths) %>% 
+#   mutate(state_name = abbr2state(state)) %>% 
+#   select(-state, -url, -deaths, -rate)
+# 
+# final_data <- full_join(final_data, suicide_data)
+# 
+# write_csv(final_data, "raw-data/final_data.csv")
 
+final_data_map <- final_data_map %>% 
+  group_by(state_name, year)
 
-final_data_map <- full_join(final_data, us_states)
 
 # Creating Map of Firearm Death Rates per State
 
@@ -39,8 +60,54 @@ firearm_graphic
 # Making another graphic
 
 death_graphic <- final_data %>%
-  ggplot(aes(x = state_name, y = total_deaths)) + geom_col()
+  filter(year == 2017) %>% 
+  filter(cause == "All Intents Firearm") %>% 
+  ggplot(aes(x = reorder(state_name, -deaths_year), y = deaths_year)) + 
+  geom_col(width = 0.5) +
+  theme(
+    axis.text.x = element_text(angle = 50, hjust = .6, vjust = .6)
+  ) +
+  labs(
+    title = "Number of Deaths By Guns By State",
+    x = "State",
+    y = "Number of Deaths"
+  )
 
 death_graphic
+
+
+# Making a statistical model of the data
+
+stat_model <- final_data %>% 
+  filter(cause == "All Intents Firearm") %>%
+  group_by(state_name) %>% 
+  nest() %>%
+  mutate(mod = map(data, ~ lm(deaths_year ~ suicide_rate, data = .x))) %>%
+  mutate(coefficients = map(mod, ~ tidy(.x))) %>%
+  mutate(sum_stats = map(mod, ~ glance(.x))) %>%
+  unnest(coefficients) %>%
+  select(-std.error, -statistic, -p.value) %>%
+  unnest(sum_stats) 
+  
+
+
+# Scatter Plot
+
+final_data_suicide <- final_data %>% 
+  group_by(state_name, year)
+
+final_data %>% 
+  group_by(state_name, year) %>% 
+  ggplot(aes(x = suicide_rate, y = deaths_year)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Deaths per Year by Guns by Suicide Rate of State",
+    x = "Suicide Rate per 1000",
+    y = "Deaths per Year"
+  )
+  
+  
+
 
 
--- milestone8_shiny/app.R
+++ milestone8_shiny/app.R
@@ -11,13 +11,13 @@ ui <- fluidPage(
                tabPanel("Firearm Death Rate",
                         selectInput(inputId = "year",  # Give the input a name "genotype"
                                     label = "Select Year",  # Give the input a label to be displayed in the app
-                                    choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015,"2014" = 2014, "2005" = 2005), selected = "a"),
+                                    choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015, "2014" = 2014, "2005" = 2005), selected = "a"),
                         plotOutput("firearm_plot")
                ),
                 tabPanel("A Different Graphic",
                          selectInput(inputId = "year2",  # Give the input a name "genotype"
                                      label = "Select Year",  # Give the input a label to be displayed in the app
-                                     choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015,"2014" = 2014, "2005" = 2005), selected = "a"),
+                                     choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015, "2014" = 2014, "2005" = 2005), selected = "a"),
                          plotOutput("suicide_plot")
                 ),
                tabPanel("About",
@@ -31,7 +31,7 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$firearm_plot <- renderPlot(ggplot(data = final_data_map[final_data_map$year == input$year2,],
+    output$firearm_plot <- renderPlot(ggplot(data = final_data_map[final_data_map$year == input$year,],
                                      mapping = aes(x = long, y = lat, group = group, fill = rate_per_1000)) + 
                                   geom_polygon(color = "gray90", size = 0.1) +
                                   coord_map(projection = "albers", 
