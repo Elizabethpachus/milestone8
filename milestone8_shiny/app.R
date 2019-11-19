f7105ff239eb764910a2c623491ceb00e23f2561
Elizabeth Pachus <epachus@college.harvard.edu>
2019-11-19 11:33:31 -0500

    Created second plot

--- milestone8_shiny/app.R
+++ milestone8_shiny/app.R
@@ -10,13 +10,17 @@ ui <- fluidPage(
     navbarPage("Navbar",
                tabPanel("Firearm Death Rate",
                         selectInput(inputId = "year",  # Give the input a name "genotype"
-                                    label = "1. Select genotype",  # Give the input a label to be displayed in the app
+                                    label = "Select Year",  # Give the input a label to be displayed in the app
                                     choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015,"2014" = 2014, "2005" = 2005), selected = "a"),
                         plotOutput("firearm_plot")
                ),
                 tabPanel("A Different Graphic",
+                         selectInput(inputId = "year2",  # Give the input a name "genotype"
+                                     label = "Select Year",  # Give the input a label to be displayed in the app
+                                     choices = c("2017" = 2017, "2016" = 2016, "2015" = 2015,"2014" = 2014, "2005" = 2005), selected = "a"),
+                         plotOutput("suicide_plot")
                 ),
-               tabPanel("Map",
+               tabPanel("About",
                         includeMarkdown("about.md")
                )
     )
@@ -27,13 +31,31 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$firearm_plot <- renderPlot(ggplot(data = final_data_map[final_data_map$year == input$year,],
+    output$firearm_plot <- renderPlot(ggplot(data = final_data_map[final_data_map$year == input$year2,],
                                      mapping = aes(x = long, y = lat, group = group, fill = rate_per_1000)) + 
                                   geom_polygon(color = "gray90", size = 0.1) +
-                                  coord_map(projection = "albers", lat0 = 39, lat1 = 45) + labs(title = "Firearm Mortality by State") + theme_map() + labs(fill = "Death Rate per 100,000") + scale_fill_gradient(low = "#ffcccb", high = "#CB454A") +
-                                  labs(title = "2017 Firearm Mortality by State") +
-                                  theme(legend.position = "right", plot.title = element_text(hjust = 0.5))
-    )    
+                                  coord_map(projection = "albers", 
+                                            lat0 = 39, lat1 = 45) + 
+                                      labs(title = "Firearm Mortality by State") + 
+                                      theme_map() + 
+                                      labs(fill = "Death Rate per 100,000") + 
+                                            scale_fill_gradient(low = "#ffcccb", 
+                                            high = "#CB454A") +
+                                  labs(title = "Firearm Mortality by State",
+                                       subtitle = "Firearm Death Rate Per 1000 People",
+                                       caption = "Data from CDC") +
+                                  theme(legend.position = "right",
+                                        plot.title = element_text(hjust = 0.5))
+    )
+    output$suicide_plot <- renderPlot(ggplot(data = final_data_suicide[final_data_suicide$year == input$year2,], aes(x = suicide_rate, y = deaths_year)) +
+                                          geom_point() +
+                                          geom_smooth(method = "lm") +
+                                          labs(
+                                              title = "Deaths per Year by Guns by Suicide Rate of State",
+                                              x = "Suicide Rate per 1000",
+                                              y = "Deaths per Year"
+                                          )
+    )
 }
 
 # Run the application 
